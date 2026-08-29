2f418530cfa3ccba0a77bdbdcefaa8b8e2b4c460
nawaab2028-sobee <nawaab2028@gmail.com>
2026-08-29 23:27:09 +0530

    Update start.py

--- cantarella/start.py
+++ cantarella/start.py
@@ -36,9 +36,9 @@ REACTIONS = [
 ]
 
 
-dev_text = "👨‍💻 Mind Behind This Bot:\n• @SmartBoy_ApnaMS\n• @TeamCinderella"
+dev_text = "👨‍💻 Mind Behind This Bot:\n• @DmOwner\n• @akaza7902"
 expected_dev_hash = "b9e63b7578bdec13f3cb3162fe5f5e93dccaba3bfd5c8ddacbb90ffdcdcce402"
-channels_text = "📢 Official Channels:\n• @PW_SENSEI\n• @TeamCinderella\n\nStay updated for new features!"
+channels_text = "📢 Official Channels:\n• @THEUPDATEDGUYS\n• @ReX_update\n\nStay updated for new features!"
 expected_channels_hash = "e19212e571bd0f6626450dd790029d392c0748c554d4b386a0c0752f4148d37d"
 
 if (
